82871b9155b5ac7c7d9e4deb9749654078038e77
absarah <abbywilliams789@gmail.com>
2022-03-30 15:37:09 -0600

    Printable view

--- fuelcolor/app/classes/controller/cc.php
+++ fuelcolor/app/classes/controller/cc.php
@@ -4,7 +4,7 @@
 *RETRIEVED from home.php
 */
 class Controller_cc extends Controller_Template
-{
+{	
 
 	/**
 	 * The basic welcome message
@@ -15,31 +15,33 @@ class Controller_cc extends Controller_Template
 
 	/*public $template = 'template_admin';*/
 
-	public function action_index(){
+	public function action_index() {
 		$data = array();
 		$this->template->title= "Home Page";
 		$this->template->content= View::forge('m1/index.php', $data);
 		$this->template->css= Asset::css("M1.css");
 	}
 
-	public function action_about(){
+	public function action_about() {
 		$data = array();
 		$this->template->title= "About Us Page";
 		$this->template->content= View::forge('m1/about.php', $data);
 		$this->template->css= Asset::css("M1.css");
 	}
 
-	public function action_colorGenerator(){
+	public function action_colorGenerator() {
 		$data = array();
 		$row_col_num = "";
 		$color_num = "";
+		$submit = "";
 		$this->template->title= "Color Coordinate Generator";
 		$this->template->content= View::forge('m1/colorGenerator.php', $data);
 		$this->template->css= Asset::css("M1.css");
 
-		if (isset($_GET["num"]) && isset($_GET["colors"])) {
+		if (isset($_GET["num"]) && isset($_GET["colors"]) && isset($_GET["submit"])) {
 			$row_col_num = $_GET["num"];
 			$color_num = $_GET["colors"];
+			$submit = $_GET["submit"];
 			if (!is_numeric($row_col_num) || $row_col_num < 1 || $row_col_num > 26) {
 				echo ("Number of Rows/Columns must be between 1 and 26");
 				echo ("You entered: " . $row_col_num);
@@ -47,13 +49,18 @@ class Controller_cc extends Controller_Template
 			if (!is_numeric($color_num) || $color_num < 1 || $color_num > 10) {
 				echo ("Number of Colors must be between 1 and 10");
 				echo ("You entered: " . $color_num);
-			}
+			}			
+			return new Response(View::forge('m1/print.php', $data));
 		}
-		
-
-
-		
 		$this->template->row_col_num = $row_col_num;
 		$this->template->color_num = $color_num;
+	}
+	
+	public function action_print() {
+		$submit = "";
+		$data['title'] = "Printable View";
+		$data['content'] = "Generated Tables";
+
+		return new Response(View::forge('m1/print.php', $data));
 	}
 }
---
+++ fuelcolor/app/logs/2022/03/30.php
@@ -0,0 +1,4 @@
+<?php defined('COREPATH') or exit('No direct script access allowed'); ?>
+
+ERROR - 2022-03-30 09:06:36 --> Notice - Undefined index: submit in /s/bach/j/under/absarah/local_html/ColorCoded/fuelcolor/app/classes/controller/cc.php on line 43
+ERROR - 2022-03-30 09:06:40 --> Notice - Undefined index: submit in /s/bach/j/under/absarah/local_html/ColorCoded/fuelcolor/app/classes/controller/cc.php on line 43
