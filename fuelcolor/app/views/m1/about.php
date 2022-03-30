07ef651bdcf4cf9e3ceee81a6795eb66ef27bbc7
elitadanilyuk <elitadanilyuk@gmail.com>
2022-03-29 23:54:32 -0600

    finishing about content

--- fuelcolor/app/views/m1/about.php
+++ fuelcolor/app/views/m1/about.php
@@ -12,22 +12,26 @@
                     <li class="member-card">
                         <img class="member-img" src="../../../m1/assets/img/abby.png" alt="">
                         <h2 class="member-name">Abby Williams</h2>
-                        <p class="bio">junior  machiatto  game of thrones  major: CS  Fun fact, almost died in Coasta Rica due to sleep walking related incident</p>
+                        <p class="bio">I am a Junior majoring in Computer Science. I like to relax, recharge, and refresh with a delicious macchiato latte while bingeing <i>Game of Thrones</i>.</p>
+                        <p class="fun-fact">Fun fact: I almost died in Costa Rica due to sleep walking related incident.</p>
                     </li>
                     <li class="member-card">
                         <img class="member-img" src="../../../m1/assets/img/elita.png" alt="">
                         <h2 class="member-name">Elita Danilyuk</h2>
-                        <p class="bio">junior  latte  the office  major: CS, software engineering  Fun fact, I've made 6 wedding cakes</p>
+                        <p class="bio">I am a Junior majoring in Computer Science with a concentration in software engineering. I survive off lattes and obscene amounts of quotes from <i>The Office</i>.</p>
+                        <p class="fun-fact">Fun fact: I have made six wedding cakes (one being my own).</p>
                     </li>
                     <li class="member-card">
                         <img class="member-img" src="../../../m1/assets/img/grace.png" alt="">
                         <h2 class="member-name">Grace Fuller</h2>
-                        <p class="bio">senior  medium roast drip coffee with a splash of cream  south park  major: bio minor: CS  Fun fact, used to play guitar, bass, drums</p>
+                        <p class="bio">I am a Senior majoring in Biology and minoring in Computer Science. A medium-roast drip coffee with a splash of cream and <i>South Park</i> always seems to brighten my day.</p>
+                        <p class="fun-fact">Fun fact: I used to play the guitar, bass, and drums.</p>
                     </li>
                     <li class="member-card">
                         <img class="member-img" src="../../../m1/assets/img/maya.png" alt="">
                         <h2 class="member-name">Maya Vitrano</h2>
-                        <p class="bio">senior  matcha  adventure time  major: CS  Fun fact, middle name Margarita</p>
+                        <p class="bio">I am a Senior majoring in Computer Science. Nothing motivates me more than a gloomy rainy day while watching <i>Adventure Time</i> with a nice cup of matcha latte in hand.</p>
+                        <p class="fun-fact">Fun fact: my middle name is Margarita.</p>
                     </li>
                </ul>
             </div>
