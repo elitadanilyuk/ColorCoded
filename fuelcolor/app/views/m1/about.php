5d3e65172d00ab28881aa954050cb5d5f5ea6b5b
elitadanilyuk <elitadanilyuk@gmail.com>
2022-03-29 23:25:21 -0600

    set up of bios complete

--- fuelcolor/app/views/m1/about.php
+++ fuelcolor/app/views/m1/about.php
@@ -12,22 +12,22 @@
                     <li class="member-card">
                         <img class="member-img" src="../../../m1/assets/img/abby.png" alt="">
                         <h2 class="member-name">Abby Williams</h2>
-                        <p>junior  machiatto  game of thrones  major: CS  Fun fact, almost died in Coasta Rica due to sleep walking related incident</p>
+                        <p class="bio">junior  machiatto  game of thrones  major: CS  Fun fact, almost died in Coasta Rica due to sleep walking related incident</p>
                     </li>
                     <li class="member-card">
                         <img class="member-img" src="../../../m1/assets/img/elita.png" alt="">
                         <h2 class="member-name">Elita Danilyuk</h2>
-                        <p>junior  latte  the office  major: CS, software engineering  Fun fact, I've made 6 wedding cakes</p>
+                        <p class="bio">junior  latte  the office  major: CS, software engineering  Fun fact, I've made 6 wedding cakes</p>
                     </li>
                     <li class="member-card">
                         <img class="member-img" src="../../../m1/assets/img/grace.png" alt="">
                         <h2 class="member-name">Grace Fuller</h2>
-                        <p>senior  medium roast drip coffee with a splash of cream  south park  major: bio minor: CS  Fun fact, used to play guitar, bass, drums</p>
+                        <p class="bio">senior  medium roast drip coffee with a splash of cream  south park  major: bio minor: CS  Fun fact, used to play guitar, bass, drums</p>
                     </li>
                     <li class="member-card">
                         <img class="member-img" src="../../../m1/assets/img/maya.png" alt="">
                         <h2 class="member-name">Maya Vitrano</h2>
-                        <p>senior  matcha  adventure time  major: CS  Fun fact, middle name Margarita</p>
+                        <p class="bio">senior  matcha  adventure time  major: CS  Fun fact, middle name Margarita</p>
                     </li>
                </ul>
             </div>
