03ae9551fb31c67cba0617a7aaf362f49fe96382
elitadanilyuk <elitadanilyuk@gmail.com>
2022-03-30 19:29:17 -0600

    adding Grace's limitting code

--- fuelcolor/app/logs/2022/03/30.php
+++ fuelcolor/app/logs/2022/03/30.php
@@ -1,1 +1,2 @@
 <?php defined('COREPATH') or exit('No direct script access allowed'); ?>
+ERROR - 2022-03-30 19:24:28 --> Notice - Undefined offset: 26 in /s/bach/j/under/elita/local_html/ColorCoded/fuelcolor/app/views/m1/colorGenerator.php on line 74
