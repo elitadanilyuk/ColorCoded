c15e9c7c1a373636cd2148ace2f6ed33f01e9244
Maya Vitrano <mayavitrano@gmail.com>
2022-05-04 21:07:10 -0600

    database

--- fuelcolor/app/config/development/db.php
+++ fuelcolor/app/config/development/db.php
@@ -22,9 +22,9 @@
 return array(
 	'default' => array(
 		'connection' => array(
-			'dsn'      => 'mysql:host=localhost;dbname=fuel_dev',
-			'username' => 'root',
-			'password' => 'root',
+			'dsn'      => 'mysql:host=faure;dbname=mayavit',
+			'username' => 'mayavit',
+			'password' => '832325425',
 		),
 	),
 );
