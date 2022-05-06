8145ce31d5efd249c44d1bf286a9f875ee023180
Elita Danilyuk <71053850+elitadanilyuk@users.noreply.github.com>
2022-05-05 23:23:12 -0600

    Delete 04.php

--- fuelcolor/app/logs/2022/05/04.php
+++
@@ -1,7 +0,0 @@
-<?php defined('COREPATH') or exit('No direct script access allowed'); ?>
-
-ERROR - 2022-05-04 19:44:33 --> Error - Database type not defined in "mysql:host=faure.cs.colostate.edu" configuration or "mysql:host=faure.cs.colostate.edu" configuration does not exist in /s/bach/m/under/mayavit/local_html/ColorCoded/fuelcolor/core/classes/database/connection.php on line 72
-ERROR - 2022-05-04 20:08:32 --> 42S02 - SQLSTATE[42S02]: Base table or view not found: 1146 Table 'mayavit.users' doesn't exist with query: "SELECT * FROM `users`" in /s/bach/m/under/mayavit/local_html/ColorCoded/fuelcolor/core/classes/database/pdo/connection.php on line 235
-ERROR - 2022-05-04 20:09:22 --> Runtime Recoverable error - Object of class Fuel\Core\Database_PDO_Cached could not be converted to string in /s/bach/m/under/mayavit/local_html/ColorCoded/fuelcolor/app/views/m1/index.php on line 25
-ERROR - 2022-05-04 21:05:17 --> Runtime Recoverable error - Object of class Fuel\Core\Database_PDO_Cached could not be converted to string in /s/bach/m/under/mayavit/local_html/ColorCoded/fuelcolor/app/views/m1/index.php on line 25
-ERROR - 2022-05-04 21:06:00 --> Runtime Recoverable error - Object of class Fuel\Core\Database_PDO_Cached could not be converted to string in /s/bach/m/under/mayavit/local_html/ColorCoded/fuelcolor/app/views/m1/index.php on line 25
