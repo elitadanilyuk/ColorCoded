6e49cd3e30bc2ec7132c921c98d8eeef9c54506e
Elita Danilyuk <71053850+elitadanilyuk@users.noreply.github.com>
2022-05-05 19:08:13 -0600

    Delete 04.php

--- fuelcolor/app/logs/2022/05/04.php
+++
@@ -1,3 +0,0 @@
-<?php defined('COREPATH') or exit('No direct script access allowed'); ?>
-
-ERROR - 2022-05-04 21:34:39 --> Notice - Undefined variable: color_map in /s/bach/j/under/absarah/local_html/ColorCoded/fuelcolor/app/views/m1/colorGenerator.php on line 156
